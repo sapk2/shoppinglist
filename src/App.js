168420e459e7d1b8a281748efc6379c07d2d4f45
sapk2 <sapkotap521@gmail.com>
2024-11-05 21:49:53 +0545

    comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+
+import React, { useState } from 'react';
+import './App.css';
+const App=()=>{
+ const[items, setItem] =useState([]);
+  const [inputvalue,setInputvalue]=useState('');
+  const [error, setError] = useState('');
+
+
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  if (!inputvalue.trim()) {
+    setError('please fill the text field');
+    return;
+  }
+  setItem([...items, {text:inputvalue,id:Date.now(),isStrikethrough:false}]);
+  setInputvalue('');
+  setError('');
+
+}
+const Togglestrikethrough=(id)=>{
+  setItem(items.map(item=>item.id===id?{...item,isStrikethrough:!item.isStrikethrough}:item));
+}
+const deleteItem =(id)=>{
+  setItem(items.filter(item=>item.id!==id));
+
+}
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1> Shopping List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text'value={inputvalue} onChange={(e)=>setInputvalue(e.target.value)} />
+        <button className='formbutton' type='submit'>Add</button>
+      </form> {error && <p className="error">{error}</p>}
+     <ul>
+     {
+      items.map(item=>(
+        <li key={items.id} onDoubleClick={()=>Togglestrikethrough(items.id)} style={{textdecoration:item.isStrikethrough ? 'strikethrough':'none'}}
         >
-          Learn React
-        </a>
-      </header>
+          {item.text}
+          <button className='ul-button' onClick={()=>deleteItem(item.id)}>❌</button>
+
+       </li>
+
+      ))}
+      
+     </ul>
     </div>
   );
 }
